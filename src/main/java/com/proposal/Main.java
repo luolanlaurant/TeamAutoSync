69052ae2fe70fa3c7768c80601544baabf53e8e9
Alaurant <zhaody085@163.com>
2024-03-22 03:43:41 +1000

    change path

--- src/main/java/com/proposal/Main.java
+++ src/main/java/com/proposal/Main.java
@@ -3,7 +3,7 @@ package com.proposal;
 public class Main {
     public static void main(String[] args) {
        
-        Team team = yamlTeamLoader.loadTeam("./permissions/plugin-dark-theme.YAML");
+        Team team = yamlTeamLoader.loadTeam("permissions/plugin-dark-theme.YAML");
 
         GitHubTeamSync.syncTeamMembers(team);
     }
